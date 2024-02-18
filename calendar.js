a73e5e287258c746d7ea099493aee9019fae547a
akac97 <142359207+akac97@users.noreply.github.com>
2024-02-18 21:12:06 +0100

    Delete calendar.js

--- calendar.js
+++
@@ -1,6 +0,0 @@
-function updateCalendar() {
-    var now = new Date();
-    var date = now.getDate() + "/" + (now.getMonth()+1) + "/" + now.getFullYear();
-    document.getElementById('calendar').innerHTML = date;
-}
-updateCalendar();
